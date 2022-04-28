4ccbe44eb14706cf336d92c11dcf65da2a809487
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-04-28 20:51:17 +0530

    my first commit

---
+++ simple/src/test/java/Latest/NewTest.java
@@ -0,0 +1,19 @@
+package Latest;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+@Test
+public void Hi()
+{
+	System.out.println("Hi guys");
+}
+
+
+@Test
+public void Hello()
+{
+	System.out.println("Hello guys");
+}
+}
